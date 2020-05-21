dda4c7810b7092d00e5ff6c4557e7d8f1d8734eb
mgarg123 <mggarg771@gmail.com>
2020-05-21 13:30:34 +0530

    #UI & CSS changes

--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,30 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>GraphQL API - Covid19 API by ncovindias.xyz</title>
+        <meta name="title" content="Covid19 GraphQL API" />
+        <meta name="description" content="A GraphQL based covid19 API. Get all details like total cases, deaths, recoveries of India. Get country, state and district level details. " />
+        <meta name="keywords" content="covid19api, covid19 graphql api, graphql api, covid api, india covid19 api, corona virus api of india and world, corona api with state and district data, ncov api, covid19india api " />
+        <meta name="robots" content="index, follow" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+
+        <title>Covid19 GraphQL API | Covid19 API by ncovindias</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
       </Head>
       <header>
-        <div className="logo">
-          <img src={logo} style={{ height: "42px", width: "42px" }} alt="" />
+        <div className="left-header">
+          <div className="logo">
+            <img src={logo} style={{ height: "42px", width: "42px" }} alt="" />
+          </div>
+          <div className="brand-name">
+            <span>NCOV-INDIA</span>
+          </div>
         </div>
-        <div className="brand-name">
-          <span>NCOV-INDIA</span>
+        <div className="right-header">
+          <div className="dashboard">
+            <span><a href="https://ncovindias.xyz">Dashboard</a></span>
+          </div>
         </div>
       </header>
       <main>
@@ -119,89 +133,292 @@ export default function Home() {
                 </tbody>
               </table>
             </div>
-            <div className="available-fields-table">
-              <div>
-                <span>
-                  Available fields
+            <div className="table-container">
+              <div className="available-fields-table">
+                <div>
+                  <span>
+                    Available fields
                 </span>
-              </div>
-              <table>
-                <thead>
-                  <tr>
-                    <th>Field Name</th>
-                    <th>Description</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>cases</td>
-                    <td>Shows no. of confirmed cases</td>
-                  </tr>
-                  <tr>
-                    <td>deaths</td>
-                    <td>Shows no. of deaths</td>
-                  </tr>
-                  <tr>
-                    <td>recovered</td>
-                    <td>Shows no. of recoveries</td>
-                  </tr>
-                  <tr>
-                    <td>active</td>
-                    <td>Shows no. of patients active.</td>
-                  </tr>
-                  <tr>
-                    <td>todayCases</td>
-                    <td>Shows no. of cases for current date</td>
-                  </tr>
-                  <tr>
-                    <td>todayRecovered</td>
-                    <td>Shows no. of recoveries for current date</td>
-                  </tr>
-                  <tr>
-                    <td>todayDeaths</td>
-                    <td>Shows no. of deaths for current date</td>
-                  </tr>
-                  <tr>
-                    <td>lastupdated</td>
-                    <td>Last updated timestamp</td>
-                  </tr>
-                  <tr>
-                    <td>stateName</td>
-                    <td>Shows name of state in district query</td>
-                  </tr>
-                  <tr>
-                    <td>date</td>
-                    <td>Shows date in historical query</td>
-                  </tr>
-                  <tr>
-                    <td>stateCode</td>
-                    <td>Shows state code.</td>
-                  </tr>
-                  <tr>
-                    <td>totalAffectedStates</td>
-                    <td>Shows no. of states infected</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            {/* <div className="api-docs-container">
-              <div className="api-docs-box">
-                <div className="api-docs-title">
-                  <span>Query for all cases</span>
                 </div>
-                <div className="api-docs-code">
-                  <div>
-                    <code>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Field Name</th>
+                      <th>Description</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>cases</td>
+                      <td>Shows no. of confirmed cases</td>
+                    </tr>
+                    <tr>
+                      <td>deaths</td>
+                      <td>Shows no. of deaths</td>
+                    </tr>
+                    <tr>
+                      <td>recovered</td>
+                      <td>Shows no. of recoveries</td>
+                    </tr>
+                    <tr>
+                      <td>active</td>
+                      <td>Shows no. of patients active.</td>
+                    </tr>
+                    <tr>
+                      <td>todayCases</td>
+                      <td>Shows no. of cases for current date</td>
+                    </tr>
+                    <tr>
+                      <td>todayRecovered</td>
+                      <td>Shows no. of recoveries for current date</td>
+                    </tr>
+                    <tr>
+                      <td>todayDeaths</td>
+                      <td>Shows no. of deaths for current date</td>
+                    </tr>
+                    <tr>
+                      <td>lastupdated</td>
+                      <td>Last updated timestamp</td>
+                    </tr>
+                    <tr>
+                      <td>stateName</td>
+                      <td>Shows name of state in district query</td>
+                    </tr>
+                    <tr>
+                      <td>date</td>
+                      <td>Shows date in historical query</td>
+                    </tr>
+                    <tr>
+                      <td>stateCode</td>
+                      <td>Shows state code.</td>
+                    </tr>
+                    <tr>
+                      <td>totalAffectedStates</td>
+                      <td>Shows no. of states infected</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div className="api-framework">
+                <div>
+                  <span>Usage</span>
+                </div>
+                <div>
+                  <span>Use with any REST API framework: </span>
+                </div>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Framework/Library</th>
+                      <th>Example</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Axios</td>
+                      <td>
+                        <a href="https://medium.com/@stubailo/how-to-call-a-graphql-server-with-axios-337a94ad6cf9">Check Example</a>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Fetch</td>
+                      <td>
+                        <a href="https://moonhighway.com/fetching-data-from-a-graphql-api">Check Example</a>
 
-                    </code>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Android - Apollo Client</td>
+                      <td>
+                        <a href="https://github.com/apollographql/apollo-android">Check Example</a>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>iOS - Apollo Client</td>
+                      <td>
+                        <a href="https://www.apollographql.com/docs/ios/">Check Example</a>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+
+            <div className="api-docs-container">
+              <div className="title">Examples</div>
+              <div className="box-container">
+                <div className="box">
+                  <div className="query-title">
+                    <span>Get total cases in India</span>
+                  </div>
+                  <div className="code-demo">
+                    <pre>
+                      {'{'}<br />
+                      <span id="qry">country</span> {'{'}<br />
+                      <span id="qry">         cases</span><br />
+                      <span id="qry">         deaths</span> <br />
+                      <span id="qry">         recovered</span> <br />
+                      <span id="qry">         active</span> <br />
+                      <span id="qry">         todayCases</span> <br />
+                      <span id="qry">         todayRecovered</span> <br />
+                      <span>      {'}'}</span><br />
+                      {'}'}
+                    </pre>
+
                   </div>
                 </div>
+                <div className="box">
+                  <div className="query-title">
+                    <span>Get details of specific State/UT</span>
+                  </div>
+                  <div className="code-demo">
+                    <pre>
+                      {'{'}<br />
+                      <span id="qry">state</span> (<span id="qry">stateName</span>:"Maharashtra") {'{'}<br />
+                      <span id="qry">         cases</span><br />
+                      <span id="qry">         deaths</span> <br />
+                      <span id="qry">         recovered</span> <br />
+                      <span id="qry">         lastupdated</span> <br />
+                      <span id="qry">         todayCases</span> <br />
+                      <span id="qry">         todayRecovered</span> <br />
+                      <span>      {'}'}</span><br />
+                      {'}'}
+                    </pre>
+
+                  </div>
+                </div>
+                <div className="box">
+                  <div className="query-title">
+                    <span>Get historical cases(Daily basis) of India</span>
+                  </div>
+                  <div className="code-demo">
+                    <pre>
+                      {'{'}<br />
+                      <span id="qry">country</span> {'{'}<br />
+                      <span id="qry">         historical</span> {'{'}<br />
+                      <span id="qry">             date</span><br />
+                      <span id="qry">             cases</span><br />
+                      <span id="qry">             deaths</span> <br />
+                      <span id="qry">             recovered</span> <br />
+                      <span>        {'}'}</span><br />
+                      <span>    {'}'}</span><br />
+                      {'}'}
+                    </pre>
+
+                  </div>
+                </div>
+                <div className="box">
+                  <div className="query-title">
+                    <span>Get historical cases (Time Series) of India</span>
+                  </div>
+                  <div className="code-demo">
+                    <pre>
+                      {'{'}<br />
+                      <span id="qry">country</span> {'{'}<br />
+                      <span id="qry">         historical</span> (<span id="qry">type</span>:"timeSeries") {'{'}<br />
+                      <span id="qry">             date</span><br />
+                      <span id="qry">             cases</span><br />
+                      <span id="qry">             deaths</span> <br />
+                      <span id="qry">             recovered</span> <br />
+                      <span>        {'}'}</span><br />
+                      <span>    {'}'}</span><br />
+                      {'}'}
+                    </pre>
+
+                  </div>
+                </div>
+                <div className="box">
+                  <div className="query-title">
+                    <span>Get historical cases (Datewise) of India</span>
+                  </div>
+                  <div className="code-demo">
+                    <pre>
+                      {'{'}<br />
+                      <span id="qry">country</span> {'{'}<br />
+                      <span id="qry">         historical</span> (<span id="qry">from</span>:"31/01", <span id="qry">to</span>:"20/03") {'{'}<br />
+                      <span id="qry">             date</span><br />
+                      <span id="qry">             cases</span><br />
+                      <span id="qry">             deaths</span> <br />
+                      <span id="qry">             recovered</span> <br />
+                      <span>        {'}'}</span><br />
+                      <span>    {'}'}</span><br />
+                      {'}'}
+                    </pre>
+
+                  </div>
+                </div>
+                <div className="box">
+                  <div className="query-title">
+                    <span>Get details of all states</span>
+                  </div>
+                  <div className="code-demo">
+                    <pre>
+                      {'{'}<br />
+                      <span id="qry">states</span> {'{'}<br />
+                      <span id="qry">         name</span><br />
+                      <span id="qry">         cases</span><br />
+                      <span id="qry">         deaths</span> <br />
+                      <span id="qry">         active</span> <br />
+                      <span id="qry">         recovered</span> <br />
+                      <span id="qry">         lastupdated</span> <br />
+                      <span id="qry">         todayCases</span> <br />
+                      <span id="qry">         todayRecovered</span> <br />
+                      <span>      {'}'}</span><br />
+                      {'}'}
+                    </pre>
+
+                  </div>
+                </div>
+                <div className="box">
+                  <div className="query-title">
+                    <span>Get details of specific district</span>
+                  </div>
+                  <div className="code-demo">
+                    <pre>
+                      {'{'}<br />
+                      <span id="qry">district</span> (<span id="qry">stateName</span>:"Bihar",  <br />
+                      <span id="qry">       districtName</span>:"Patna") {'{'}<br />
+                      <span id="qry">                 cases</span><br />
+                      <span id="qry">                 deaths</span> <br />
+                      <span id="qry">                 recovered</span> <br />
+                      <span id="qry">                 todayCases</span> <br />
+                      <span id="qry">                 todayRecovered</span> <br />
+                      <span>       {'}'}</span><br />
+                      {'}'}
+                      <span>{" "}</span>
+                      <span>{" "}</span>
+                    </pre>
+
+                  </div>
+                </div>
+                <div className="box">
+                  <div className="query-title">
+                    <span>Get details of all states with districts</span>
+                  </div>
+                  <div className="code-demo">
+                    <pre>
+                      {'{'}<br />
+                      <span id="qry">states</span> {'{'}<br />
+                      <span id="qry">         districts</span> {'{'}<br />
+                      <span id="qry">             cases</span><br />
+                      <span id="qry">             deaths</span> <br />
+                      <span id="qry">             recovered</span> <br />
+                      <span id="qry">             todayCases</span> <br />
+                      <span id="qry">             todayRecovered</span> <br />
+                      <span>          {'}'}</span><br />
+                      <span>     {'}'}</span><br />
+                      {'}'}
+                    </pre>
+
+                  </div>
+                </div>
+
               </div>
-            </div> */}
+
+            </div>
 
           </div>
         </div>
-
       </main>
       <footer>
         <div className="footer-left">
