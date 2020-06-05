7edd2a7a52fa9df1081f4f45e4c04f03cd98b386
mgarg123 <mggarg771@gmail.com>
2020-06-05 20:07:55 +0530

    #Enabled Cors

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const expressGraphQL = require('express-graphql')
 import schema from '../../schema/schema'
-
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use('/', expressGraphQL({
     schema: schema,
     graphiql: true
